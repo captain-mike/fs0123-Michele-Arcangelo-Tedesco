f2732d0ea1b576d57876d2c39bb18b34d5f60ba1
captain-mike <info@grafikweb.it>
2023-02-16 18:11:28 +0100

    memory

--- m1/s3/g3/memory/js/memory.js
+++ m1/s3/g3/memory/js/memory.js
@@ -1,2 +1,57 @@
 let icone = ['😺','💥','😻','🙉','🐷','🐖', '🐧','🐸','🐉','🐛','🐜','🐝'];
 
+//aggiorna l'array icone in modo che contenga un doppione per ogni icona
+icone = icone.concat(icone);
+
+const memory = document.getElementById('memory');
+
+
+function shuffle(array){
+    
+    let newArray = [];
+    let n = array.length;
+    for(let i = 0; i < n; i++){
+        let rand = Math.floor(Math.random() * array.length);
+        newArray.push(array[rand]);
+        array.splice(rand, 1);
+    }
+    
+    return newArray;
+}
+
+function start(){
+    
+    //mescolo icone
+    icone = shuffle(icone)
+
+    /*
+    <div class="card">
+        <div class="icona">😺</div>
+    </div>
+    */
+
+    icone.forEach(el => {
+
+        //creo gli elementi
+        let card = document.createElement('div');
+        let icona = document.createElement('div');
+
+        //assegnoo le classi
+        card.classList.add('card');
+        icona.classList.add('icona');
+
+        icona.textContent = el;
+
+        card.addEventListener('click',()=>{
+            icona.classList.add('selected');
+        })
+
+        card.append(icona);
+        memory.append(card)
+
+    })
+
+}
+
+start();
+
